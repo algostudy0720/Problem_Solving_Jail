a4e9f71f9fa50c0945219ed59fe74921b4c31266
June Park <36508552+Jun0S2@users.noreply.github.com>
2022-01-18 21:18:25 +0900

    Programmers : lev2 멀쩡한사각형_PHJ

--- Programmers/lev2/멀쩡한_사각형_PHJ.java
+++ Programmers/lev2/멀쩡한_사각형_PHJ.java
@@ -11,13 +11,12 @@ public class 멀쩡한_사각형_PHJ {
 		System.out.println(solution(8,12));
 	}
 	/**
-	 * 함수
+	 * 함수 : 기울기가 12/8 인 일차함수-> y = 12/8x;
 	 * @param w
 	 * @param h
 	 * @return
 	 */
 	public static long solution(int x, int y) {//내편의성을위해..w->x로 h->y로 수정했다
-		//예시 : 기울기가 12/8 인 일차함수-> y = 12/8x;
         long answer = 0;
         for (int i = 0; i < x; i++) {// ->
             answer += y * i / x;	 //그래프 영역 더해준다
