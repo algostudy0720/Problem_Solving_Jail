4f5e47ee92eb6c4d11b85e7d8373e0275da4b159
DooHee <dh73111@gmail.com>
2022-01-23 00:06:20 +0900

    Programmers : lev2 _124나라의숫자_KDH

--- Programmers/lev2/멀쩡한사각형/멀쩡한사각형_KDH.java
+++ Programmers/lev2/멀쩡한사각형/멀쩡한사각형_KDH.java
@@ -1,34 +1,21 @@
-package Programmers;
+package Programmers.lev2;
 
-public class 멀쩡한_사각형 {
+public class _124_나라의_숫자 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		solution(8,12);
+		solution(10);
 	}
 
-	public static long solution(int w, int h) {
-		long answer = 0;
-		long a = 0, b = 0, temp = 0;
-		if(w >= h) {
-			a = (long)w;
-			b = (long)h;
-		}
-		else {
-			a = (long)w;
-			b = (long)h;
-		}
-		
-        
-		for (long i = b; i > 0; i--) {
-			if(a % i == 0 && b % i == 0) {
-				temp = i;
-				break;
-			}
-		}
-		
-        
-		answer = ((long)w*(long)h)-((long)w+(long)h-temp);
-		return answer;
+	public static String solution(int n) {
+	      String[] num = {"4","1","2"};
+	      String answer = "";
+
+	      while(n > 0){
+	          answer = num[n % 3] + answer;
+	          n = (n - 1) / 3;
+	      }
+	      System.out.println(answer);
+	      return answer;
 	}
 }
